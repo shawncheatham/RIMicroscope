9e7824ed4d7d66d37ec1801a06cf14ae0a52ff4c
Shawn Cheatham <shawn00@gmail.com>
2015-02-25 20:37:04 -0500

    Including Error notifications

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,15 @@
+Template.postSubmit.created = function() {
+  Session.set('postSubmitErrors', {});
+}
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
 // Create form for submitting a post
 Template.postSubmit.events({
   'submit form': function(e) {
@@ -8,15 +20,19 @@ Template.postSubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
+
+    var errors = validatePost(post);
+    if (errors.title || errors.url)
+      return Session.set('postSubmitErrors', errors);
     
     Meteor.call('postInsert', post, function(error, result) {
       // display the error to the user and abort
       if (error)
-        return alert(error.reason);
+        return throwError(error.reason);
 
       // show this result but route anyway
       if (result.postExists)
-        alert('This link has already been posted');
+        throwError('This link has already been posted');
       
       Router.go('postPage', {_id: result._id});  
     });
